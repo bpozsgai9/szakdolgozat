6fdbd8d800bd2917f5fa911ca8c42ce7d2d98892
pozsgai_barnabas <pozsgai.barnabas@petrik.hu>
2022-12-27 13:54:20 +0100

    folyt_kov

---
+++ graph.js
@@ -0,0 +1,45 @@
+//gráf helye
+let networkDiv = document.getElementById('myNetwork');
+let graphDiv = document.getElementById('graphDiv');
+var network = null;
+
+fetch("output.json")
+    .then(response => response.json())
+    .then(outputJson => {
+
+        let graph = {
+            nodes: new vis.DataSet(outputJson["nodes"]),
+            edges: new vis.DataSet(outputJson["edges"])
+        };
+
+        //valamiért ezt nem látja
+        //forma
+        let options = {
+            nodes: {
+                color: {
+                    border: '#77a5fc',
+                    background: '#77a5fc',
+                    highlight: {
+                        border: '#fcca03',
+                        background: '#fcca03'
+                    }
+                }
+            },
+            edges: {},
+            groups: {
+                class: {
+                    color: { background: '#77a5fc', border: '#77a5fc' },
+                    shape: 'circle'
+                },
+                method: {
+                    color: { background: '#77a5fc', border: '#77a5fc' },
+                    shape: 'triangleDown'
+                }
+            }
+        };
+
+        network = new vis.Network(networkDiv, graph, options);
+    });
+
+
+
