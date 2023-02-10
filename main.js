c562883e230cf1c0c369d4af4f095894efd9997c
pozsgai_barnabas <pozsgai.barnabas@petrik.hu>
2023-02-10 12:43:34 +0100

    get_cmd_args

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { app, BrowserWindow, dialog, Notification } = require('electron')
 const path = require('path')
-const fs = require('fs');
+const fs = require('fs')
 const appIcon = 'src/pic/osa.ico'
+
+//cmd args
+const process = require('process')
+let args = process.argv
 
 const createWindow = () => {
   const window = new BrowserWindow({
@@ -12,63 +16,84 @@ const createWindow = () => {
     },
     icon: appIcon
   })
-  
+
   //értesítés
-  let sourceNotification = new Notification({ 
+  let sourceNotification = new Notification({
     title: 'Notification from OSA:\nLoad Source File',
     body: 'Please choose a source file to analyze!',
-    icon:  appIcon
+    icon: appIcon
   })
 
-  //TODO:Ezt nem jelzi ki, innen folytatás
-  let outputNotification = new Notification({ 
+
+  let outputNotification = new Notification({
     title: 'Notification from OSA:\nLoad Graph File',
     body: 'Please choose a json file to analyze!',
-    icon:  appIcon
+    icon: appIcon
   })
 
   //fájl másolása
   function copyFileByResponse(response, source) {
     if (!response.canceled) {
       fs.copyFile(response.filePaths[0], path.join(__dirname, source), (err) => {
-        
+
         if (err) throw err;
         console.log('Copied!');
       });
 
     } else {
-        console.log("No file selected!");
-        app.quit()
+      console.log("No file selected!");
+      app.quit()
     }
   }
 
-  //forrás dialóg
-  sourceNotification.show()
-  dialog.showOpenDialog(window, {properties: ['openFile']})
-  .then(sourceRes => copyFileByResponse(sourceRes, 'src/source/source.java'))
-  .then(()  => { 
+  function copyFileBySource(from, to) {
+    fs.copyFile(from, path.join(__dirname, to), (err) => {
 
-    //gráf dialóg
-    outputNotification.show()
-    dialog.showOpenDialog(window, {properties: ['openFile']})
-    .then(graphRes => copyFileByResponse(graphRes, 'src/source/output.json'))
-    .then(()  => window.loadFile('view/index.html'))
-  })
+      if (err) throw err;
+      console.log('Copied!');
+    });
+  }
+
+  //forrás dialóg, ha nincs arg
+  if (args[1] == null && args[2] == null) {
+
+    sourceNotification.show()
+    dialog.showOpenDialog(window, { properties: ['openFile'] })
+      .then(sourceRes => copyFileByResponse(sourceRes, 'src/source/source.java'))
+      .then(() => {
+
+        //gráf dialóg
+        outputNotification.show()
+        dialog.showOpenDialog(window, { properties: ['openFile'] })
+          .then(graphRes => copyFileByResponse(graphRes, 'src/source/output.json'))
+          .then(() => window.loadFile('view/index.html'))
+      })
+
+  } else {
+
+    new Promise((resolve) => {
+      copyFileBySource(args[1], 'src/source/source.java')
+      resolve();
+    })
+    .then(() => copyFileBySource(args[2], 'src/source/output.json'))
+    .finally(() => window.loadFile('view/index.html'))
+
+  }
 }
 
 app.whenReady()
-.then(() => {
-  createWindow()
+  .then(() => {
+    createWindow()
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
   })
-})
 
 app.on('window-all-closed', () => {
-  
+
   fs.unlinkSync(path.join(__dirname, 'src/source/source.java'))
-  fs.unlinkSync(path.join(__dirname,'src/source/output.json'))
+  fs.unlinkSync(path.join(__dirname, 'src/source/output.json'))
   if (process.platform !== 'darwin') app.quit()
 })
 
